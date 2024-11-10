eed8012f529774b67caee98fa8cc7f66554b621e
Андрей Слёзкин <slezkin@mail.ru>
2024-11-10 22:21:29 +0300

    добавлен API для srv2

--- hw4/srv2/src/main/java/otus/srv2/Srv2Controller.java
+++ hw4/srv2/src/main/java/otus/srv2/Srv2Controller.java
@@ -1,11 +1,31 @@
 package otus.srv2;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Srv2Controller {
+
+
+    @PostMapping("/user")
+    public String postUser(){
+        return "POST user";
+    }
+
+    @GetMapping("/user/{userId}")
+    public String getUser(){
+        return "GET user";
+    }
+
+    @PutMapping("/user/{userId}")
+    public String putUser(){
+        return "PUT user";
+    }
+
+    @DeleteMapping("/user/{userId}")
+    public String deleteUser(){
+        return "DELETE user";
+    }
 
     @GetMapping("/health/")
     public String healthCheck(){
